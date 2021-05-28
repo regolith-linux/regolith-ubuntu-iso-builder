4d9607aa2643242b33b98776e479f9fedd079fa1
Ken Gilmer <kgilmer@gmail.com>
2021-05-28 13:07:26 -0700

    Add regolith class to grub config. From https://github.com/regolith-linux/regolith-desktop/issues/508.

--- scripts/build.sh
+++ scripts/build.sh
@@ -161,12 +161,12 @@ insmod all_video
 set default="0"
 set timeout=30
 
-menuentry "${GRUB_LIVEBOOT_LABEL}" {
+menuentry "${GRUB_LIVEBOOT_LABEL}" --class regolith {
    linux /casper/vmlinuz boot=casper nopersistent toram quiet splash ---
    initrd /casper/initrd
 }
 
-menuentry "${GRUB_INSTALL_LABEL}" {
+menuentry "${GRUB_INSTALL_LABEL}" --class regolith {
    linux /casper/vmlinuz boot=casper only-ubiquity quiet splash ---
    initrd /casper/initrd
 }
